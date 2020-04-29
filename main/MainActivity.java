5cf40d648947209dc94b44b87d57f8b663e4ff1a
Ankit152 <ankitkurmi152@gmail.com>
2020-04-29 22:45:11 +0530

    added toasty

--- main/MainActivity.java
+++ main/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 import java.util.Random;
 
+import es.dmoral.toasty.Toasty;
+
 public class MainActivity extends AppCompatActivity {
     private Button btn;
     private ImageView image_view;
@@ -30,43 +32,30 @@ public class MainActivity extends AppCompatActivity {
     }
     private void dice_roll(){
         int rand_num=random.nextInt(6)+1;
-        Toast toast;
         switch(rand_num){
             case 1:
                 image_view.setImageResource(R.drawable.dice1);
-                toast=Toast.makeText(this,"1", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
+                Toasty.normal(this,"1", Toast.LENGTH_SHORT).show();
                 break;
             case 2:
                 image_view.setImageResource(R.drawable.dice2);
-                toast = Toast.makeText(this, "2", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
+                Toasty.normal(this, "2", Toast.LENGTH_SHORT).show();
                 break;
             case 3:
                 image_view.setImageResource(R.drawable.dice3);
-                toast=Toast.makeText(this,"3", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
+                Toasty.normal(this,"3", Toast.LENGTH_SHORT).show();
                 break;
             case 4:
                 image_view.setImageResource(R.drawable.dice4);
-                toast=Toast.makeText(this,"4", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
+                Toasty.normal(this,"4", Toast.LENGTH_SHORT).show();
                 break;
             case 5:
                 image_view.setImageResource(R.drawable.dice5);
-                toast=Toast.makeText(this,"5", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
+                Toasty.normal(this,"5", Toast.LENGTH_SHORT).show();
                 break;
             case 6:
                 image_view.setImageResource(R.drawable.dice6);
-                toast=Toast.makeText(this,"6", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM,0,0);
-                toast.show();
+                Toasty.normal(this,"6", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
